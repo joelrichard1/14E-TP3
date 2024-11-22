bc60a26dfbf99cfb213725151fe48b1425d01e8a
joel.richard1@outlook.com <joel.richard1@outlook.com>
2024-11-22 17:02:23 -0500

    Ajout des fonctionnalités de visualisation graphique de la serre, modification de l'état des systèmes et de l'affichage ainsi que la mise à jour des conditions ambiantes de la serre.

--- TP2_14E_A24-main/Views/GreenHouseWindow.xaml.cs
+++ TP2_14E_A24-main/Views/GreenHouseWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Automate.Utils;
+using Automate.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +24,7 @@ namespace Automate.Views
         public GreenHouseWindow()
         {
             InitializeComponent();
+            DataContext = new GreenHouseViewModel();
         }
     }
 }
