9665332f985b7d61d58f2cb0fc9c68664a635c2e
joel.richard1@outlook.com <joel.richard1@outlook.com>
2024-12-17 16:10:29 -0500

    Essai test qui passe.

--- AutomateTests/ViewModels/GreenHouseViewModelTests.cs
+++ AutomateTests/ViewModels/GreenHouseViewModelTests.cs
@@ -16,9 +16,8 @@ namespace Automate.Tests
         {
             var viewModel = new GreenHouseViewModel();
 
-            Assert.AreEqual("21 °C", viewModel.Temperature);
+            Assert.AreEqual("20 °C", viewModel.Temperature);
         }
-
 
         [Test]
         public void Constructor_ShouldInitializeHumidity()
